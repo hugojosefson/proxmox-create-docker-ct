f684e743c5d51f8b13210f5f9772b852a2d774d0
Hugo Josefson <hugo@josefson.org>
2022-05-28 20:58:55 +0200

    feat: print error codes

--- usr_local_bin/create-docker-ct-files/run.ts
+++ usr_local_bin/create-docker-ct-files/run.ts
@@ -1,45 +1,49 @@
-import { asString, isString } from "./fn.ts";
+import { asString, isString, j } from "./fn.ts";
 
 export async function run(
   command: string | string[],
   stdin?: string,
 ): Promise<string> {
-  const pipeStdIn = isString(stdin);
-
   const cmd: string[] = isString(command) ? command.split(" ") : command;
-  const process = Deno.run({
-    cmd,
-    stdin: pipeStdIn ? "piped" : "null",
-    stdout: "piped",
-    stderr: "piped",
-  });
+  const pipeStdIn = isString(stdin);
+  try {
+    const process = Deno.run({
+      cmd,
+      stdin: pipeStdIn ? "piped" : "null",
+      stdout: "piped",
+      stderr: "piped",
+    });
 
-  if (pipeStdIn) {
-    const stdinBuf = new TextEncoder().encode(stdin);
-    try {
-      await process.stdin?.write(stdinBuf);
-    } finally {
-      await process.stdin?.close();
+    if (pipeStdIn) {
+      const stdinBuf = new TextEncoder().encode(stdin);
+      try {
+        await process.stdin?.write(stdinBuf);
+      } finally {
+        await process.stdin?.close();
+      }
     }
-  }
 
-  const [
-    status,
-    stdout,
-    stderr,
-  ] = await Promise.all([
-    process.status(),
-    process.output(),
-    process.stderrOutput(),
-  ]);
-  process.close();
+    const [
+      status,
+      stdout,
+      stderr,
+    ] = await Promise.all([
+      process.status(),
+      process.output(),
+      process.stderrOutput(),
+    ]);
+    process.close();
 
-  if (status.success) {
-    return asString(stdout);
+    if (status.success) {
+      return asString(stdout);
+    }
+    const reason = {
+      ...status,
+      stderr: asString(stderr),
+    };
+    return Promise.reject(reason);
+  } catch (error) {
+    console.error(j({ cmd, error }));
+    throw error;
   }
-  const reason = {
-    ...status,
-    stderr: asString(stderr),
-  };
-  return Promise.reject(reason);
 }
