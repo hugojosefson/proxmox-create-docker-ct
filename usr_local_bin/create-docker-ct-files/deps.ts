aa04061a5e06a6faf58200f64a0f53178ab3ba88
Hugo Josefson <hugo@josefson.org>
2022-05-29 22:24:18 +0200

    refactor: delete unused allocateVolume

--- usr_local_bin/create-docker-ct-files/deps.ts
+++ usr_local_bin/create-docker-ct-files/deps.ts
@@ -5,7 +5,3 @@ export { camelCase } from "https://deno.land/x/case@2.1.1/mod.ts";
 export { Select } from "https://deno.land/x/cliffy@v0.24.2/prompt/select.ts";
 export { fetch as fetchFile } from "https://deno.land/x/file_fetch@0.2.0/mod.ts";
 export { default as _parseColumns } from "https://cdn.skypack.dev/pin/parse-columns@v3.0.0-7lgB0zjFrTuoTzGqeI3T/mode=imports/optimized/parse-columns.js";
-export {
-  default as execAll,
-  type Match,
-} from "https://cdn.skypack.dev/execall@v3.0.0?dts";
