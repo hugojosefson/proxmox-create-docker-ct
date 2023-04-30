34add144c6097435ad675c4d7a7af53e95a9f49f
Hugo Josefson <hugo@josefson.org>
2023-04-30 21:47:12 +0200

    refactor: delete dead code

--- usr_local_bin/create-docker-ct-files/parse-columns.ts
+++ usr_local_bin/create-docker-ct-files/parse-columns.ts
@@ -1,17 +1,9 @@
 import { parseJsonSafe } from "./fn.ts";
 import { _parseColumns, camelCase, mapKeys, mapValues } from "./deps.ts";
-import { run } from "./run.ts";
 
 export function parseColumns<T>(input: string): T[] {
   const rows: T[] = _parseColumns(input) as T[];
   return rows
     .map((row) => mapKeys(row as Readonly<Record<string, unknown>>, camelCase))
     .map((row) => mapValues(row, parseJsonSafe) as T);
-}
-
-async function _withHeaders(input: string, headers: string[]): Promise<string> {
-  return await run(
-    ["column", "--table", "--table-columns", ...headers.join(",")],
-    { stdin: input },
-  );
 }
